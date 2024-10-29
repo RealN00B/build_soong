ab9610bea687bbdeaa46a183456990531fb55c7d
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-29 17:55:37 +0800

    gen_build_prop: Always return release-keys

--- scripts/gen_build_prop.py
+++ scripts/gen_build_prop.py
@@ -40,9 +40,6 @@ def get_build_flavor(product_config):
   return build_flavor
 
 def get_build_keys(product_config):
-  default_cert = product_config.get("DefaultAppCertificate", "")
-  if default_cert == "" or default_cert == os.path.join(TEST_KEY_DIR, "testKey"):
-    return "test-keys"
   return "release-keys"
 
 def override_config(config):
